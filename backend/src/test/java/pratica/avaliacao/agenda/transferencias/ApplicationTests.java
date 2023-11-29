498ea167d719fcacf41ef868f9b4dd9a61518146
Samuel Quaresma <squoliver@gmail.com>
2023-11-28 21:05:56 -0300

    First commit

---
+++ backend/src/test/java/pratica/avaliacao/agenda/transferencias/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pratica.avaliacao.agenda.transferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
