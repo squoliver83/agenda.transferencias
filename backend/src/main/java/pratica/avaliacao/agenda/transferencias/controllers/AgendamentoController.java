acfea96b91cc0787d0f3e5dc4a598c1bc62331e9
Samuel Quaresma <squoliver@gmail.com>
2023-11-29 23:43:54 -0300

    Add CORS, ajuste de rota e porta de acesso ao backend

--- backend/src/main/java/pratica/avaliacao/agenda/transferencias/controllers/AgendamentoController.java
+++ backend/src/main/java/pratica/avaliacao/agenda/transferencias/controllers/AgendamentoController.java
@@ -16,7 +16,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/agendamentos")
+@RequestMapping(value = "/api/agendamentos")
 public class AgendamentoController {
 
     @Autowired
